f4ceaa6f1b7ad502c71ac2c9765272379e4a214a
Kar46 <karam.sayed@vit.edu.in>
2025-08-02 17:05:59 +0530

    added background image overlay

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const global = {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 async function displayPopularMovies() {
-  const { results } = await fetchAPIKey('movie/popular'); // destructure results from the API response object to get the array of movies
+  const { results } = await fetchApiData('movie/popular'); // destructure results from the API response object to get the array of movies
   console.log(results);
   results.forEach((movie) => {
     if (movie.adult === true) {
@@ -69,7 +69,7 @@ async function displayPopularMovies() {
 // displayPopularMovies();
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 async function displayPopularShows() {
-  const { results } = await fetchAPIKey('tv/popular'); // destructure results from the API response object to get the array of shows
+  const { results } = await fetchApiData('tv/popular'); // destructure results from the API response object to get the array of shows
   console.log(results);
   results.forEach((show) => {
     if (show.adult === true) {
@@ -140,8 +140,9 @@ async function displayPopularShows() {
 async function movieDetails() {
   console.log(window.location.search.split('=')[1]);
   const id = window.location.search.split('=')[1];
-  const results = await fetchAPIKey(`movie/${id}`);
+  const results = await fetchApiData(`movie/${id}`);
   console.log('movie detail', results);
+  backgroundImage('movie', results.backdrop_path);
   const detail = document.createElement('div');
   detail.innerHTML = `<div class="details-top">
           <div>
@@ -207,9 +208,78 @@ async function movieDetails() {
         </div>`;
   document.querySelector('#movie-details').appendChild(detail);
 }
-// movieDetails();
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function fetchAPIKey(endpoint) {
+async function tvDetails() {
+  console.log(window.location.search.split('=')[1]);
+  const id = window.location.search.split('=')[1];
+  const result = await fetchApiData(`tv/${id}`);
+  backgroundImage('show', result.backdrop_path);
+  console.log('tv detail', result);
+  const detail = document.createElement('div');
+  detail.innerHTML = `<div class="details-top">
+            <div>
+            ${
+              result.poster_path
+                ? `
+              <img
+                src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+                class="card-img-top"
+                alt="Movie Title" />`
+                : `<img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="Movie Title" />`
+            }
+            </div >
+            <div>
+              <h2>${result.name}</h2>
+              <p>
+                <i class="fas fa-star text-primary"></i>
+                ${result.vote_average.toFixed(1)} / 10
+              </p>
+              <p class="text-muted">Air Date: ${result.first_air_date}</p>
+              <p>
+                ${result.overview}
+              </p>
+              <h5>Genres</h5>
+              <ul class="list-group">
+              ${result.genres
+                .map((genre) => {
+                  return `<li>${genre.name}</li>`;
+                })
+                .join('')}
+
+              </ul>
+              <a href="${
+                result.homepage
+              }" target="_blank" class="btn">Visit Show Homepage</a>
+            </div>
+          </div>
+          <div class="details-bottom">
+            <h2>Show Info</h2>
+            <ul>
+              <li><span class="text-secondary">Number of Episodes:</span> ${addCommasToNumber(
+                result.number_of_episodes
+              )}</li>
+              <li><span class="text-secondary">Last Episode To Air:</span> ${
+                result.last_episode_to_air.name
+              }</li>
+              <li><span class="text-secondary">Status:</span> ${
+                result.status
+              }</li>
+            </ul>
+            <h4>Production Companies</h4>
+            <div class="list-group">${result.production_companies
+              .map((company) => {
+                return `<span>${company.name}</span>`;
+              })
+              .join(', ')}</div>
+          </div>`;
+  document.querySelector('#show-details').appendChild(detail);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function fetchApiData(endpoint) {
   const key = '2222c7541cbbb37ecfbd5587d4554f78';
   const url = 'https://api.themoviedb.org/3';
   showSpinner();
@@ -252,7 +322,7 @@ function init() {
       break;
 
     case '/tv-details.html':
-      console.log('tvDetailsPage');
+      tvDetails();
       break;
   }
   highlightLink();
@@ -272,4 +342,27 @@ function showSpinner() {
 function hideSpinner() {
   const spinner = document.querySelector('.spinner');
   spinner.classList.remove('show');
+}
+
+async function backgroundImage(type, backgroundPath) {
+  showSpinner();
+  const overlay = document.createElement('div');
+  overlay.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlay.style.backgroundSize = 'cover';
+  overlay.style.position = 'absolute';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.right = '0';
+  overlay.style.bottom = '0';
+  overlay.style.zIndex = '-1';
+  overlay.style.opacity = '0.1';
+  overlay.style.backgroundPosition = 'center';
+  overlay.style.backgroundRepeat = 'no-repeat';
+  hideSpinner();
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlay);
+  } else {
+    document.querySelector('#show-details').appendChild(overlay);
+  }
 }
